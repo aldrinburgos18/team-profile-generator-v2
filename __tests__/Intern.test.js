ca2c0e7ce5bc49bb4e2eb91358d6e6bab331a462
aldrinburgos <burgos.aldrin@gmail.com>
2022-02-25 20:31:15 -0600

    add intern getSchool() method and test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -13,3 +13,14 @@ test("creates an intern object", () => {
   expect(intern.email).toBe("burgos.aldrin@gmail.com"),
     expect(intern.school).toBe("2University");
 });
+
+test("gets intern's school", () => {
+  const intern = new Intern(
+    "Aldrin",
+    1,
+    "burgos.aldrin@gmail.com",
+    "2University"
+  );
+
+  expect(intern.getSchool()).toEqual(expect.stringContaining("2University"));
+});
