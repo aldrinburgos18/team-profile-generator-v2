257d2b8b54b84c083996741271a938fb85615b42
aldrinburgos <burgos.aldrin@gmail.com>
2022-02-25 20:22:39 -0600

    add engineer getGitHub() method and test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("@jest/globals");
 const Engineer = require("../lib/Engineer");
 
 test("creates an engineer object", () => {
@@ -12,4 +13,17 @@ test("creates an engineer object", () => {
   expect(engineer.id).toEqual(expect.any(Number));
   expect(engineer.email).toBe("burgos.aldrin@gmail.com");
   expect(engineer.github).toBe("aldrinburgos18");
+});
+
+test("gets engineer's github account", () => {
+  const engineer = new Engineer(
+    "Aldrin",
+    1,
+    "burgos.aldrin@gmail.com",
+    "aldrinburgos18"
+  );
+
+  expect(engineer.getGitHub()).toEqual(
+    expect.stringContaining("aldrinburgos18")
+  );
 });
