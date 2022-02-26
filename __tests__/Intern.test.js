804abdb8ed189a9878a8922628dc9b76661712ca
aldrinburgos <burgos.aldrin@gmail.com>
2022-02-25 20:28:35 -0600

    create an intern object and test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,15 @@
+const Intern = require("../lib/Intern");
+
+test("creates an intern object", () => {
+  const intern = new Intern(
+    "Aldrin",
+    1,
+    "burgos.aldrin@gmail.com",
+    "2University"
+  );
+
+  expect(intern.name).toBe("Aldrin");
+  expect(intern.id).toEqual(expect.any(Number));
+  expect(intern.email).toBe("burgos.aldrin@gmail.com"),
+    expect(intern.school).toBe("2University");
+});
