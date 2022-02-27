f3502b261e01cc7c355398c8c63850ed47c24d5a
aldrinburgos <burgos.aldrin@gmail.com>
2022-02-26 20:32:26 -0600

    add questions/refactor code

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 function init() {
-  console.log(`
-=============
-HELLO MANAGER!
-=============`);
   return inquirer
     .prompt([
       {
@@ -23,18 +22,26 @@ HELLO MANAGER!
         message: "What is your e-mail address?",
       },
       {
-        type: "confirm",
-        name: "confirmAddEmployee",
-        message: "Would you like to add a team member?",
-        default: true,
+        type: "input",
+        name: "officeNbr",
+        message: "What is your office number?",
       },
     ])
     .then((employeeData) => {
-      if (employeeData.confirmAddEmployee) {
-        return addTeamMember(employeeData);
+      //create employees array
+      if (!employeeData.employees) {
+        employeeData.employees = [];
       }
 
-      return employeeData;
+      const manager = new Manager(
+        employeeData.name,
+        employeeData.id,
+        employeeData.email,
+        employeeData.officeNbr
+      );
+
+      employeeData.employees.push(manager);
+      return addTeamMember(employeeData);
     });
 }
 
@@ -43,26 +50,105 @@ function addTeamMember(employeeData) {
     .prompt([
       {
         type: "list",
-        name: "teamMemberSelect",
-        message: "Which team member would you like to add?",
-        choices: ["Engineer", "Intern"],
+        name: "selectEmployee",
+        message: "Would you like to add more team members?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "No, I don't want to add another team member",
+        ],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: ({ selectEmployee }) =>
+          `What is your ${selectEmployee}'s name?`,
+        when: ({ selectEmployee }) => {
+          if (selectEmployee === "Engineer" || selectEmployee === "Intern") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: ({ selectEmployee }) =>
+          `What is your ${selectEmployee}'s ID number?`,
+        when: ({ selectEmployee }) => {
+          if (selectEmployee === "Engineer" || selectEmployee === "Intern") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: ({ selectEmployee }) =>
+          `What is your ${selectEmployee}'s e-mail address?`,
+        when: ({ selectEmployee }) => {
+          if (selectEmployee === "Engineer" || selectEmployee === "Intern") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your Engineer's GitHub username?",
+        when: ({ selectEmployee }) => {
+          if (selectEmployee === "Engineer") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school does your Intern go to?",
+        when: ({ selectEmployee }) => {
+          if (selectEmployee === "Intern") {
+            return true;
+          } else {
+            return false;
+          }
+        },
       },
     ])
-    .then((teamMemberData) => {
-      if (teamMemberData.teamMemberSelect === "Engineer") {
-        addEngineer(employeeData);
+    .then((employee) => {
+      if (employee.selectEmployee === "Engineer") {
+        //push engineer into employees array
+        const engineer = new Engineer(
+          employee.name,
+          employee.id,
+          employee.email,
+          employee.github
+        );
+
+        employeeData.employees.push(engineer);
+        return addTeamMember(employeeData);
+      } else if (employee.selectEmployee === "Intern") {
+        //push intern into employees array
+        const intern = new Intern(
+          employee.name,
+          employee.id,
+          employee.email,
+          employee.school
+        );
+
+        employeeData.employees.push(intern);
+        return addTeamMember(employeeData);
       } else {
-        addIntern(employeeData);
+        return employeeData;
       }
     });
 }
 
-function addEngineer(employeeData) {
-  console.log(employeeData);
-}
-
-function addIntern(employeeData) {
-  console.log(employeeData);
-}
-
-init().then((data) => console.log(data));
+init().then((data) => console.log(data.employees));
