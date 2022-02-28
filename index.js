399388fe51536a8832977083659a711faaf85f45
aldrinburgos <burgos.aldrin@gmail.com>
2022-02-27 21:35:42 -0600

    add function to generate page

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const promptUser = require("./utils/promptUser");
+const generatePage = require("./src/page-template");
+const fs = require("fs");
 
-promptUser().then((data) => console.log(data.employees));
+promptUser()
+  .then((data) => {
+    const employeeData = data.employees;
+    const manager = employeeData[0];
+    var engineers = employeeData.filter(
+      (employee) => employee.role == "Engineer"
+    );
+    var interns = employeeData.filter((employee) => employee.role == "Intern");
+    return generatePage(manager, engineers, interns);
+  })
+  .then((pageContent) => console.log(pageContent));
